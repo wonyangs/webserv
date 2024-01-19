2f964bff55623d67541824b5be621bb24aa180d9
jeongmin <yesuel1111@naver.com>
2024-01-19 18:18:18 +0900

    chore: Config.cpp에 예외 코드 메시지 추가

--- utils/Config.cpp
+++ utils/Config.cpp
@@ -6,9 +6,31 @@ const std::map<int, std::string> Config::statusMessages =
 std::map<int, std::string> Config::initializeStatusMessages(void) {
   std::map<int, std::string> messages;
   messages[200] = "OK";
+  messages[201] = "Created";
+  messages[204] = "No Content";
+  messages[206] = "Partial Content";
+  messages[300] = "Multiple Choices";
+  messages[301] = "Moved Permanently";
+  messages[302] = "Found";
+  messages[304] = "Not Modified";
+  messages[307] = "Temporary Redirect";
   messages[400] = "Bad Request";
+  messages[401] = "Unauthorized";
+  messages[403] = "Forbidden";
   messages[404] = "Not Found";
-  messages[405] = "Http Not Allowed";
+  messages[405] = "Method Not Allowed";
+  messages[406] = "Not Acceptable";
+  messages[408] = "Request Timeout";
+  messages[409] = "Conflict";
+  messages[410] = "Gone";
+  messages[413] = "Payload Too Large";
+  messages[414] = "URI Too Long";
+  messages[415] = "Unsupported Media Type";
+  messages[429] = "Too Many Requests";
   messages[500] = "Internal Server Error";
+  messages[501] = "Not Implemented";
+  messages[502] = "Bad Gateway";
+  messages[503] = "Service Unavailable";
+  messages[504] = "Gateway Timeout";
   return messages;
 }
