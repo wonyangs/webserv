543b09d7c823385b52887e4fbe640775e3c024e7
jeongmin <yesuel1111@naver.com>
2024-01-22 23:04:16 +0900

    feat: Util 클래스에 splitVector 함수 구현
    - 중복된 사용으로 Util 함수에 구현

--- utils/Util.cpp
+++ utils/Util.cpp
@@ -5,3 +5,39 @@ std::string const Util::itos(int n) {
   ss << n;
   return ss.str();
 }
+
+stringVec Util::splitVector(octetVec const& vec, char del) {
+  stringVec result;
+
+  std::string data(vec.begin(), vec.end());
+  std::stringstream ss(data);
+  std::string token;
+
+  while (std::getline(ss, token, del)) {
+    result.push_back(token);
+  }
+
+  if (result.size() == 0) {
+    throw StatusException(HTTP_BAD_REQUEST,
+                          "[] Util: splitVector - vector is empty");
+  }
+  return result;
+}
+
+stringVec Util::splitVectorOnce(octetVec const& vec, char del) {
+  stringVec result;
+
+  std::string data(vec.begin(), vec.end());
+  size_t pos = data.find(del);
+
+  if (pos != std::string::npos) {
+    result.push_back(data.substr(0, pos));
+    result.push_back(data.substr(pos + 1));
+  }
+
+  if (result.size() == 0) {
+    throw StatusException(HTTP_BAD_REQUEST,
+                          "[] Util: splitVectorOnce - vector is empty");
+  }
+  return result;
+}
--- utils/Util.hpp
+++ utils/Util.hpp
@@ -1,12 +1,21 @@
 #ifndef __UTIL_HPP__
 #define __UTIL_HPP__
 
+#include <algorithm>
 #include <sstream>
 #include <string>
+#include <vector>
+
+#include "../utils/StatusException.hpp"
+
+typedef std::vector<u_int8_t> octetVec;
+typedef std::vector<std::string> stringVec;
 
 class Util {
  public:
   static std::string const itos(int n);
+  static stringVec splitVector(octetVec const& vec, char del);
+  static stringVec splitVectorOnce(octetVec const& vec, char del);
 
  private:
   Util(void);
