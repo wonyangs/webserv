0b87cf55a2a10ce41a1975acf2c4f67172305195
jeongmin <yesuel1111@naver.com>
2024-01-22 15:32:10 +0900

    feat: ON_SEND 상태인 경우 응답 전송

--- server/ServerManager.cpp
+++ server/ServerManager.cpp
@@ -334,7 +334,9 @@ void ServerManager::handleReadEvent(int eventFd, Connection& connection) {
 // - 이벤트 처리 과정 중 예외 발생 가능
 void ServerManager::handleWriteEvent(int eventFd, Connection& connection) {
   try {
-    connection.send();
+    if (connection.getConnectionStatus() == Connection::ON_SEND) {
+      connection.send();
+    }
 
     if (connection.getConnectionStatus() == Connection::CLOSE) {
       removeConnection(eventFd);
