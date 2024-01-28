b586f6ec3fcbc759f0a56ad032e3b2dbf1859095
gemstoneyang <bennyws99@gmail.com>
2024-01-28 21:44:15 +0900

    feat: 상대경로를 제거하여 반환하는 removeDotSegments 구현

--- utils/Util.cpp
+++ utils/Util.cpp
@@ -17,3 +17,36 @@ int Util::stoi(std::string const& str) {
   }
   return n;
 }
+
+// path에서 .과 ..을 계산하여 반환
+// - ..로 인해 /보다 뒤로 갈 경우 /로 고정
+std::string const Util::removeDotSegments(std::string const& path) {
+  std::string inputBuffer = path;
+  std::string outputBuffer;
+
+  while (!inputBuffer.empty()) {
+    std::size_t segmentEnd = inputBuffer.find('/', 1);
+    std::string segment = segmentEnd != std::string::npos
+                              ? inputBuffer.substr(0, segmentEnd)
+                              : inputBuffer;
+
+    if (segment == "/." || segment == ".") {
+      // pass
+    } else if (segment == "/.." || segment == "..") {
+      std::size_t pos = outputBuffer.find_last_of('/');
+      if (pos != std::string::npos) {
+        outputBuffer.erase(pos);
+      }
+    } else if (!segment.empty() && segment != "/") {
+      outputBuffer += segment;
+    }
+
+    if (segmentEnd != std::string::npos) {
+      inputBuffer.erase(0, segmentEnd);
+    } else {
+      inputBuffer.clear();
+    }
+  }
+
+  return outputBuffer.empty() ? "/" : outputBuffer;
+}
