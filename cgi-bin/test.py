66f509305dcb3651ebd99a8b054508286c55756c
gemstoneyang <bennyws99@gmail.com>
2024-01-28 16:15:19 +0900

    chore: 사용하지 않는 파일 삭제 및 이미지 이동

--- cgi-bin/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-print("Content-Type: text/html", end='\r\n\r\n')
-
-print("<html><head><title>Simple Python CGI</title></head>")
-print("<body>")
-print("<h1>Hello, world!</h1>")
-print("</body></html>")
