8ef3999605293a96ae402fdfc6107897b0e301eb
gemstoneyang <bennyws99@gmail.com>
2024-01-28 15:05:45 +0900

    refactor: upload.py 코드 개선
    - 응답 출력 부분 함수화
    - 업로드할 폴더가 없는 경우 500 응답
    - return이 사용된 부분을 sys.exit()으로 수정

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -3,22 +3,29 @@ import cgitb
 import os
 import sys
 
+def print_response(status_code, status_message, body):
+    print(f"Status: {status_code} {status_message}", end='\r\n')
+    if status_code == 400 or status_code == 500:
+        print("Connection: close", end='\r\n')
+    else:
+        print("Connection: keep-alive", end='\r\n')
+    print("Content-Type: text/plain", end="\r\n\r\n")
+    print(body, end='\r\n')
+    sys.exit()
+
 cgitb.enable()
 
 # "fileUpload" 필드에서 파일 데이터 가져오기
 form = cgi.FieldStorage()
-
-if 'fileUpload' in form and fileitem.filename:
+if 'fileUpload' in form:
     fileitem = form['fileUpload']
-    fn = os.path.basename(fileitem.filename) # 디렉토리 부분 제거, 파일 이름만 추출
-    file_path = os.environ["UPLOAD_PATH"].rstrip('/') + "/" + fn
+    if fileitem.filename:
+        fn = os.path.basename(fileitem.filename)  # 디렉토리 부분 제거, 파일 이름만 추출
+        file_path = os.environ["UPLOAD_PATH"].rstrip('/') + "/" + fn
+    else:
+        print_response(400, "Bad Request", "No file uploaded.")
 else:
-    print("Status: 400 Bad Request")  # 잘못된 형식
-    print("Connection: close")
-    print("Content-Type: text/plain", end="\r\n\r\n")
-    print("Invalid field value.")
-    return
-
+    print_response(400, "Bad Request", "Invalid field value.")
 
 # HTTP 요청 메서드 가져오기
 request_method = os.environ.get("REQUEST_METHOD", "GET")
@@ -26,26 +33,14 @@ request_method = os.environ.get("REQUEST_METHOD", "GET")
 # 파일 생성 (POST 요청)
 if request_method == "POST":
     if os.path.exists(file_path):
-        print("Status: 409 Conflict")  # 파일이 이미 존재하는 경우
-        print("Connection: keep-alive")
-        print("Content-Type: text/plain", end="\r\n\r\n")
-        print("File already exists.")
+        print_response(409, "Conflict", "File already exists.")
     else:
         try:
             with open(file_path, 'wb') as f:
                 f.write(fileitem.file.read())
-            print("Status: 201 Created")  # 파일 업로드 성공
-            print("Connection: keep-alive")
-            print("Content-Type: text/plain", end="\r\n\r\n")
-            print("File created.")
+            print_response(201, "Created", "File created.")
         except OSError as e:
-            print("Status: 500 Internal Server Error")  # 파일 생성 실패
-            print("Connection: close")
-            print("Content-Type: text/plain", end="\r\n\r\n")
-            print(f"Error creating file: {e}")
+            print_response(500, "Internal Server Error", "Error creating file")
 
 else:
-    print("Status: 400 Bad Request")  # 지원하지 않는 메서드
-    print("Connection: close")
-    print("Content-Type: text/plain", end="\r\n\r\n")
-    print("Unsupported method.")
+    print_response(400, "Bad Request", "Unsupported method.")
